d79e74d6ee012344b33f5aaa80ecef41939b6f2b
achow-sta <allison.chow26@ycdsbk12.ca>
2024-12-19 15:26:56 -0500

    changed Console window size and title

--- CPTAllison.java
+++ CPTAllison.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class CPTAllison{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("AC Math Training Program", 1280, 720);
 		
 		con.println("hello world!");
 	}
