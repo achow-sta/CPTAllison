4e7d2e3585525e5a5a4226956aae6312322907fc
achow-sta <allison.chow26@ycdsbk12.ca>
2025-01-07 09:54:55 -0500

    Main Screen Created
    Visuals for the main menu screen created (no actual functions yet)

--- CPTAllison.java
+++ CPTAllison.java
@@ -1,9 +1,38 @@
 import arc.*;
+import java.awt.*;
+import java.awt.image.*;
 
 public class CPTAllison{
 	public static void main(String[] args){
 		Console con = new Console("AC Math Training Program", 1280, 720);
+		BufferedImage imghello = con.loadImage("wavingpika.gif");
 		
-		con.println("hello world!");
+		//Loop for entering empty lines
+		int intEnterCount;
+		for(intEnterCount = 0; intEnterCount <= 7; intEnterCount++){
+			con.println();
+		}
+		
+		
+		con.println("                                    Welcome to the Math Training Game!");
+		
+		//More Empty Enter Lines
+		for(intEnterCount = 0; intEnterCount <= 7; intEnterCount++){
+			con.println();
+		}
+		
+		//Load Image
+		con.drawImage(imghello, 100, 100);
+		
+		con.println("                                                   Play");
+		con.println();
+		con.println("                                                Highscores");
+		con.println();
+		con.println("                                                   Help");
+		con.println();
+		con.println("                                                   Quit");
+		
+		con.println();
+		con.println("                                        Enter a Key Letter to Begin");
 	}
 }
