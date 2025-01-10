035da632bc8be6583f17f289afcbdcb1804cb351
achow-sta <allison.chow26@ycdsbk12.ca>
2025-01-10 15:57:14 -0500

    Updated CPTAllison.java file
    - added method to ask questions one by one, check if answer entered is correct, update final score to highscores file

--- CPTAllison.java
+++ CPTAllison.java
@@ -45,16 +45,17 @@ public class CPTAllison{
 		//con.println(strKeyIn);
 		//con.println(chrKeyIn);
 		
-		
-		if(chrKeyIn == 'p' || chrKeyIn == 'P'){
-			con.clear();
-			con.setBackgroundColor(Color.BLACK);
-			playgame(con);
+		while(chrKeyIn != 'q' || chrKeyIn != 'Q'){
+			if(chrKeyIn == 'p' || chrKeyIn == 'P'){
+				con.clear();
+				con.setBackgroundColor(Color.BLACK);
+				chrKeyIn = playgame(con);
+			}
 		}
 		
 		
 	}
-	public static void playgame(Console con){
+	public static char playgame(Console con){
 		
 		//ask for username
 		String strName;
@@ -68,9 +69,10 @@ public class CPTAllison{
 			testfile.println("Perfect Squares (Advanced)");			
 			testfile.close(); //FIND WAY TO REMOVE ADVANCED PERFECT SQUARES AFTER USER COMPLETE TEST 
 			
+			con.sleep(700);
 			con.println("Perfect Squares (Advanced) has been added to your test options!");
 		}
-		con.sleep(700);
+		
 		con.clear();
 		
 		//Entering Empty Lines (Formatting)
@@ -106,7 +108,9 @@ public class CPTAllison{
 		strChosenTest = con.readLine();
 		chrChosen = strChosenTest.charAt(0);
 		
+		con.clear();
 		
+		char chrKeyIn;
 		//Open Respective Test Files
 		String strQuestions[][];
 		
@@ -119,7 +123,11 @@ public class CPTAllison{
 			//send to another method to sort randomizer
 			strQuestions = sort(strQuestions, strChosenTest);
 			
+			//send to method to ask questions
+			chrKeyIn = asking(strQuestions, strChosenTest, con, strName);
+			
 			binaryfile.close();
+			return chrKeyIn;
 			
 		}else if(chrChosen == 'P' || chrChosen == 'p'){
 			TextInputFile pfsqfile = new TextInputFile("Perfect Squares.txt");
@@ -130,7 +138,11 @@ public class CPTAllison{
 			//send to another method to sort randomizer
 			strQuestions = sort(strQuestions, strChosenTest);
 			
+			//send to method to ask questions
+			chrKeyIn = asking(strQuestions, strChosenTest, con, strName);
+			
 			pfsqfile.close();
+			return chrKeyIn;
 			
 		}else if(chrChosen == 'Q' || chrChosen == 'q'){
 			TextInputFile quadfile = new TextInputFile("Quadratic.txt");
@@ -141,7 +153,11 @@ public class CPTAllison{
 			//send to another method to sort randomizer
 			strQuestions = sort(strQuestions, strChosenTest);
 			
+			//send to method to ask questions
+			chrKeyIn = asking(strQuestions, strChosenTest, con, strName);
+			
 			quadfile.close();			
+			return chrKeyIn;
 			
 		}else if(strName.equalsIgnoreCase("statitan") && strChosenTest.equalsIgnoreCase("Perfect Squares (Advanced)")){
 			TextInputFile advpfsq = new TextInputFile("Perfect Squares (Advanced)");
@@ -152,12 +168,18 @@ public class CPTAllison{
 			//send to another method to sort randomizer
 			strQuestions = sort(strQuestions, strChosenTest);
 			
+			//send to method to ask questions
+			chrKeyIn = asking(strQuestions, strChosenTest, con, strName);
+			
 			advpfsq.close();
+			return chrKeyIn;
 		}else{
 			System.out.println("invalid keyboard input");
+			strChosenTest = con.readLine();
+			chrChosen = strChosenTest.charAt(0);
 		}
 				
-		
+		return 'm';
 	}
 	
 public static String[][] questionload(String strChosenTest){
@@ -171,15 +193,6 @@ public static String[][] questionload(String strChosenTest){
 	
 	//Count how many questions
 	TextInputFile test = new TextInputFile(strChosenTest + ".txt");
-	
-	//while(test.eof() == false){
-		//strQuestion = test.readLine();
-		//intNumQuest = intNumQuest + 1;      // <-- number of questions counter
-		//strAns1 = test.readLine();
-		//strAns2 = test.readLine();
-		//strAns3 = test.readLine();
-	//}
-	//test.close();
 	intNumQuest = questnum(strChosenTest);
 	
 	//Create Array
@@ -284,4 +297,104 @@ public static String[][] questionload(String strChosenTest){
 		
 		return strQuestions;
 	}
+	
+	public static char asking(String[][] strQuestions, String strChosenTest, Console con, String strName){
+		int intNumQuest;
+		int intCount;
+		double dblPercent = 0;
+		int intAnsCount = 0;
+		String strAnswer;
+		int intCorrect = 0;
+		
+		//get number of questions
+		intNumQuest = questnum(strChosenTest);
+		
+		//print questions one by one
+		for(intCount = 0; intCount < intNumQuest - 1; intCount++){
+			
+			//print title bar 
+			con.println("                              " + strName + "  |  " + strChosenTest + "  |  " + intCorrect + "/" + intNumQuest + "   " + dblPercent + "%");
+		
+			//print questions one by one
+			con.println(strQuestions[intCount][0]);
+			strAnswer = con.readLine();
+			
+			while(!strAnswer.equalsIgnoreCase(strQuestions[intCount][1]) && !strAnswer.equalsIgnoreCase(strQuestions[intCount][2]) && !strAnswer.equalsIgnoreCase(strQuestions[intCount][3])){
+				con.println();
+				con.println("Incorrect, try again...");
+				con.println();
+				intAnsCount = intAnsCount + 1;
+				
+				//ask question again
+				con.println(strQuestions[intCount][0] + " ");
+				strAnswer = con.readLine();
+				
+			}
+			
+			if(strAnswer.equalsIgnoreCase(strQuestions[intCount][1]) || strAnswer.equalsIgnoreCase(strQuestions[intCount][2]) || strAnswer.equalsIgnoreCase(strQuestions[intCount][3])){
+				con.println("Correct!");
+				con.sleep(500);
+				
+				
+				//only add 1 to correct answers if took one time to correctly answer
+				if(intAnsCount == 0){
+					dblPercent = intCount + 1;
+					dblPercent = Math.round(dblPercent /intNumQuest * 10000)/100;
+					intCorrect = intCorrect + 1;
+				}
+				
+				intAnsCount = 0;
+				con.clear();
+			}
+		}
+		
+		//print test complete screen
+		con.println(strChosenTest + " Complete!");
+		con.println();
+		
+		if(dblPercent >= 80){
+			con.println("Well Done " + strName + "!");
+		}else{
+			con.println("Keep Practicing " + strName + "!");
+		}
+		
+		con.println();
+		con.println("You got " + dblPercent + "%");
+		
+		//update score to highscores file
+		TextOutputFile highscore = new TextOutputFile("highscores.txt");
+		highscore.println(strName);
+		highscore.println(strChosenTest);
+		highscore.println(dblPercent);
+		
+		con.println();
+		con.println("Score Updated to Highscores Leaderboard!");
+		
+		
+		//enter empty lines (for formating)
+		int intEnterCount;
+		for(intEnterCount = 0; intEnterCount <= 4; intEnterCount++){
+			con.println();
+		}
+		
+		//return to main menu
+		con.println("Press (m) to return to Main Menu ");
+		
+		char chrKeyIn;
+		chrKeyIn = con.readChar();
+		
+		
+		while(chrKeyIn != 'm' || chrKeyIn != 'M'){
+			System.out.println("invalid keyboard input");
+			chrKeyIn = con.readChar();
+		}
+		if(chrKeyIn == 'H' || chrKeyIn == 'h'){
+			return chrKeyIn;
+		}
+		
+		return chrKeyIn;
+		
+		
+	}
+	
 }
